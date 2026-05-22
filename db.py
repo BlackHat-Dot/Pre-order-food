0f2f3cd27a22e8af1fc16b785284ea2f7c1d597b
BlackHat-Dot <vijayzidane321@gmail.com>
2026-05-22 11:25:10 +0530

    chore: reset alembic migrations for fresh database deployment

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:nXDslfkImWkfmrYsJghJyVQfoRDMPExK@yamabiko.proxy.rlwy.net:38097/railway"
+DATABASE_URL = "postgresql://postgres:oMjBkgEMtNWAOLgdTXZyVPZAHuKNyEHE@kodama.proxy.rlwy.net:50298/railway"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
